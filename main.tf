642df0f3acf31c9ef215b662ef7f50e7c6b1ad96
sreedhar <sreedhar70755@gmail.com>
2023-08-23 20:53:45 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_ssm_parameter" "main" {
   for_each = var.parameters
   name  = each.key
-  type  ="string"
+  type  = "string"
   value = each.value["value"]
 }
 
 
 variable "parameters" {
   default = {
-    "para" = { type="string", value = "docdb" }
+    "parameter" = { type="string", value = "docdb" }
   }
 }

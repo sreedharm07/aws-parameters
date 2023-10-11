55b47583fc60f5c384d19a68477df812822d1b78
sreedhar <sreedhar70755@gmail.com>
2023-10-11 12:31:53 +0530

    tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "main" {
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
-  key_id = "arn:aws:kms:us-east-1:120752001195:key/90368eb1-f0ed-4e4f-bd9f-5c38abef1dd7"
+  key_id = "arn:aws:kms:us-east-1a:120752001195:key/90368eb1-f0ed-4e4f-bd9f-5c38abef1dd7"
 }
 
 ##
